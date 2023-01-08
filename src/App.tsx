e6e99a29db5bddce7b161c4005332a33e9052a0e
abnish123kumar <abnishkrsingh10@gmail.com>
2023-01-08 15:45:25 +0530

    DegreeOfSeperation

--- src/App.tsx
+++ src/App.tsx
@@ -11,53 +11,20 @@ function App() {
 
    const[result, setResult] = useState<string[]>([]);
 
-    // function addValues():void{
-       
-    // }
     
 
-   let connections:{name:string, friends: string[]}[] = [
-    {
-      name: "sameer",
-      friends: ["aayushi", "kamalnath"]
-    },
-    {
-      name: "aayushi",
-      friends: ["bhaskar"]
-    },
-    {
-      name: "kamalnath",
-      friends: ["shanti"]
-    },
-    {
-      name: "shanti",
-      friends: ["bhaskar"]
-    }
-  ]
    
-  function connectionsListToGraph(connections:{name:string, friends: string[]}[]) {
-    const Graph:any = {}
+   
  
-   
-    
-    for (let {name, friends} of connections) {
-     
-      Graph[name] = friends ;
-    }
-    return Graph
-  }
-  
 
   function degreeOfSepration(source:string, target:string) {
   
-   // const Graph = newConnection//connectionsListToGraph(connections)
-    //console.log(Graph);
     let newConnection :any = {}
     for(let i=0; i<localStorage.length; i++){
       newConnection[localStorage.key(i)||" "]= JSON.parse(localStorage.getItem(localStorage.key(i)||" ")||" ");
    }
    console.log(newConnection);
-    //console.log(newConnection);
+    
     const connectionPaths:string[] = []
   
     function findConnections(source:string, target:string, path:any = [source], visited:any = {}) {
@@ -82,44 +49,27 @@ function App() {
    
  
  const addValues = ()=>{
-//   const addArray = [{
-//     name:`${user1}`,
-//     friends : [`${user2}`]           
-// }];
-  // localStorage.setItem("name",JSON.stringify(addArray) )
-  //       //const arr = localStorage.getItem("name");
-  //       var newArray = JSON.parse(localStorage.getItem("name") || "");
-  //      res= newArray?newArray.map((item:any)=>{
-  //           if(item.name=== user1){
-  //             item.friends.push(user1);
-  //           }
-  //       }):newArray.push({name:`${user1}`, friends:[`${user2}`]})
 
-  //      console.log(res); 
-  //localStorage.clear();
     if(localStorage.getItem(`${user1}`)){ 
-        // for(let i=0; i<localStorage.length; i++){
-        //    newConnection[localStorage.key(i)||" "]= JSON.parse(localStorage.getItem(localStorage.key(i)||" ")||" ");
-        // }
-        // console.log(newConnection);
+        
       console.log("mundhkehdkj")
         let jstion = (JSON.parse(localStorage.getItem(`${user1}`)||" "));
          console.log(jstion);
          console.log(jstion.includes(`${user2}`))
        if(jstion.includes(`${user2}`)){ 
            console.log("hero");
-          // jstion.push(`${user2}`)
+         
       localStorage.setItem(`${user1}`,JSON.stringify(jstion))
        }else{
         jstion.push(`${user2}`)
         localStorage.setItem(`${user1}`,JSON.stringify( jstion))
        }
-       // localStorage.clear();
+       
       }
        else 
        localStorage.setItem(`${user1}`, JSON.stringify([user2]));
 
-      // localStorage.clear();
+      
  }
 
   return (
